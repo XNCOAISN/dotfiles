f16dd517367814f1442e3cd279b55c3727e2dd01
XNCOAISN <39142826+XNCOAISN@users.noreply.github.com>
2026-05-10 17:50:58 +0900

    Refactor flake.nix for personal tools configuration

--- flake.nix
+++ flake.nix
@@ -1,44 +1,42 @@
 {
-  description = "My minimal Home Manager config";
+  description = "Personal tools";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, nix-darwin, home-manager, ... }:
+  outputs = { nixpkgs, ... }:
     let
-      system = "aarch64-darwin";
-      username = "te";
-      homeDirectory = "/Users/te";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      homeConfigurations.${username} =
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ./home.nix
-          ];
-        };
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-      darwinConfigurations.${username} = nix-darwin.lib.darwinSystem {
-        inherit system pkgs;
-        modules = [
-          ./darwin.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = { ... }: {
-              imports = [ ./home.nix ];
-            };
-          }
-        ];
-      };
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f (import nixpkgs { inherit system; })
+        );
+    in {
+      packages = forAllSystems (pkgs:
+        let
+          personalPackages = with pkgs; [
+            neovim
+            ripgrep
+            fd
+            fzf
+            tmux
+            git
+            gh
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            vscode
+          ];
+        in {
+          default = pkgs.buildEnv {
+            name = "personal-tools";
+            paths = personalPackages;
+          };
+        });
     };
 }
